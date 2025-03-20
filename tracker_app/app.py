935fef0006f495d829153013981e44fad8ce3d51
carlos lopez <krlosaren@gmail.com>
2025-03-19 22:49:53 -0300

    [ADD] add systemd start point

--- tracker_app/app.py
+++ tracker_app/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
+import requests
+import loguru
 
 app = FastAPI()
 
 
 @app.get("/file")
-def get_file():
+def get_file(url: str):
     """return a file when click on URL"""
     file_path = "./files/Descuentos_SENATI_Colaboradores_Marzo 2025.docx"
+    loguru.logger.info(f"file_path: {file_path}")
+    loguru.logger.info(f"url: {url}")
+    response = requests.get(url)
     return FileResponse(path=file_path, filename=file_path)
